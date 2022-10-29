e1c28e34c81bff05d8ed0266eb294295e416bab8
Margarita Villaseñor <maggie.villasenor@hotmail.com>
2022-10-28 19:52:43 -0500

    ci:Displaying stolinski data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,31 @@
 //comprobar correcto funcionamiento de .js con .html
 console.log("APIs");
-
+const button = document.getElementById('btn');
+button.addEventListener("click", displayUser('stolinski'));
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-//modificación del nombre de variables : name, blog, location
-const name = document.querySelector('name');
-const blog = document.querySelector('#blog');
-const location = document.querySelector('.location');
+//modificación del nombre de variables : n, b, l
+const n = document.querySelector('.name');
+const b= document.querySelector('.blog');
+const l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) {  //api fetch function
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+  response.json().then((data) => {
+    console.log(data);
+    n.textContent = data.name;
+    b.textContent = data.blog;
+    l.textContent = data.location;
+  }).catch(error => {
+    console.log(error);
+  })
+};
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
   n.textContent = `Algo salió mal: ${err}`
-}
+};
 
 displayUser('stolinski').catch(handleError);
